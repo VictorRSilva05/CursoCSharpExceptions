058d22827a22b40ff68f81fcd5e810af18f7312c
Victor R. Silva <victorr.silva005@gmail.com>
2024-02-10 10:58:43 -0300

    Created classes and main program

---
+++ ConsoleApp15/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp15.Exceptions;
+
+namespace ConsoleApp15.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawnLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawnLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawnLimit = withdrawnLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp15.Exceptions;
+using ConsoleApp15.Entities;
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data:");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine());
+            Console.Write("Withdrawl limit: ");
+            double withdrawlLimit = double.Parse(Console.ReadLine());
+
+            Account account = new Account(number,holder,initialBalance,withdrawlLimit);
+
+            Console.Write("\nEnter amount for withdraw: ");
+            account.Withdraw(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("New balance: " + account.Balance);
+        }
+    }
+}
